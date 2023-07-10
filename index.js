d3eb8a9bf342b8fffd4c9e3f766aea67de95161f
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-07-10 12:11:36 -0600

    added circles

--- index.js
+++ index.js
@@ -10,23 +10,33 @@ const shapes = [
 ];
 
 const render = () => {
-  const html = shapes.map((shape) => {
+  const htmlSquares = shapes.map((shape) => {
     return `<div class='${shape.color} ${shape.size}'></div>`;
   }).join(``);
 
-  document.querySelector(`ul`).innerHTML = html;
+  document.querySelector(`ul`).innerHTML = htmlSquares;
+
+  // add code that fills the new unordered list with circles
+  const circlesHTML = shapes.map((shape) => {
+    return `<div class='${shape.color} ${shape.size} circles'></div>`
+  }).join(``);
+
+  document.querySelector(`.circles`).innerHTML = circlesHTML;
 }
 
 render();
 
 const interval = setInterval(() => {
-  const color = [`red`, `green`][Math.floor(Math.random()  * 2)];
-  const size = [`sm`, `md`, `lg`][Math.floor(Math.random() * 3)];
+  // modify the application to support yellow shapes
+  const color = [`red`, `green`, `yellow`][Math.floor(Math.random()  * 3)];
+  // modify the application to support a new size with a class of xl which will be 100px
+  const size = [`sm`, `md`, `lg`, `xl`][Math.floor(Math.random() * 4)];
 
   shapes.push({ color, size });
   render();
 
-  if(shapes.length === 20) {
+  // modify the application so that it will generate up to 50 of each shape
+  if(shapes.length === 50) {//           SHOULD BE 50
     clearInterval(interval);
   }
 }, 1000);
